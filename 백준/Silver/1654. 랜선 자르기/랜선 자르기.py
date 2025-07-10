3fa3d425ce68db0cb8850a1de629562ccd91284e
Kangjunseok09 <kangjunsuk0503@gmail.com>
2025-07-10 22:26:01 +0900

    [Silver II] Title: 랜선 자르기, Time: 92 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
@@ -4,12 +4,12 @@ k, n = map(int, input().split())
 list = [int(input()) for i in range(k)]
 
 start = 1
-end = list[0]
+end = max(list)
 while start <= end:
   lanline = 0
   mid = (start + end) // 2
-  for i in range(k):
-    lanline += list[i] // mid 
+  for i in list:
+    lanline += i // mid 
   if lanline >= n:
     start = mid + 1
   else:
