9b7a9f30c62d0608999e11078c09c0a4d720b2fb
강준석 <kangjunsuk0503@gmail.com>
2025-07-28 15:07:54 +0900

    [Silver IV] Title: 에라토스테네스의 체, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Silver/2960. 에라토스테네스의 체/에라토스테네스의 체.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main(void){
+  int n, k, cnt = 0;
+
+  scanf("%d %d", &n, &k);
+  int arr[n+1];
+  for (int i = 0; i < n+1; i++) arr[i] = 0;
+
+  for(int i = 2; i < n+1; i++){
+    for(int j = i; j < n+1; j+=i){
+      if(arr[j] == 0){
+        arr[j] = 1;
+        cnt ++;
+        if(cnt == k){
+          printf("%d\n", j);
+          return 0;
+        }
+      }
+    }
+  }
+}
