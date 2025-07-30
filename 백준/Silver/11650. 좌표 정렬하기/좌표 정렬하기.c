fd0b32910be2a5cdeacbef45f9d346dafc907b2a
강준석 <kangjunsuk0503@gmail.com>
2025-07-30 15:54:09 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 60 ms, Memory: 2556 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int compare(const void *a, const void *b) {
+  int *pa = (int *)a;
+  int *pb = (int *)b;
+
+  if (pa[0] != pb[0]) return pa[0] - pb[0];   
+  else return pa[1] - pb[1];                  
+}
+
+int main(void) {
+  int n;
+  scanf("%d", &n);
+
+  int arr[100000][2];
+
+  for (int i = 0; i < n; i++) {
+    scanf("%d %d", &arr[i][0], &arr[i][1]);
+  }
+
+  qsort(arr, n, sizeof(arr[0]), compare);
+
+  for (int i = 0; i < n; i++) {
+    printf("%d %d\n", arr[i][0], arr[i][1]);
+  }
+
+  return 0;
+}
