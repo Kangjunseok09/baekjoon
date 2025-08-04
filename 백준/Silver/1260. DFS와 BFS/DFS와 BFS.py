44059f508b12d09fcfc6c6aaaf497796a476cf71
강준석 <kangjunsuk0503@gmail.com>
2025-08-04 20:58:06 +0900

    [Silver II] Title: DFS와 BFS, Time: 300 ms, Memory: 33544 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,39 +1,36 @@
 n, m, v = map(int, input().split())
-graph = [[] for i in range(n+1)]
+graph = [[] for _ in range(n + 1)]
 
-for i in range(m):
-  x, y = map(int, input().split())
-  graph[x].append(y)
-  graph[y].append(x)
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
 
-for i in range(1, n+1):
-  graph[i].sort()
+for edges in graph:
+    edges.sort()
 
-def dfs(v):
-  stack = [v]
-  visited = [False] * (n+1)
-  while stack:
-    node = stack.pop()
-    if not visited[node]:
-      print(node, end=" ")
-      visited[node] = True
-      for next in reversed(graph[node]):
+def dfs(node, visited):
+    visited[node] = True
+    print(node, end=' ')
+    for next in graph[node]:
         if not visited[next]:
-          stack.append(next)
+            dfs(next, visited)
 
-def bfs(v):
-  queue = [v]
-  visited = [False] * (n+1)
-  visited[v] = True
-  
-  while queue:
-    q = queue.pop(0)
-    print(q, end=" ")
-    for next in graph[q]:
-      if not visited[next]:
-        visited[next] = True
-        queue.append(next)
+def bfs(start):
+    visited = [False] * (n + 1)
+    queue = [start]
+    visited[start] = True
 
-dfs(v)
+    while queue:
+        node = queue[0]
+        del queue[0]
+        print(node, end=' ')
+        for next in graph[node]:
+            if not visited[next]:
+                visited[next] = True
+                queue.append(next)
+
+visited_dfs = [False] * (n + 1)
+dfs(v, visited_dfs)
 print()
 bfs(v)
