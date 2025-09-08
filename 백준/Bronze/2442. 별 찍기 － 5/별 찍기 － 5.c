9e80ceca0b46f5963652b19dff504deae483b216
강준석 <kangjunsuk0503@gmail.com>
2025-09-08 22:15:19 +0900

    [Bronze III] Title: 별 찍기 - 5, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/2442. 별 찍기 － 5/별 찍기 － 5.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main(void){
+    
+    int n;
+    scanf("%d", &n);
+    
+    for(int i = 1; i <= n; i++){
+        for(int j = i; j < n; j++){
+            printf(" ");
+        }
+        for(int j = 0; j < i * 2 - 1; j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    
+    
+    return 0;
+}
